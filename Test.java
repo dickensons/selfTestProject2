29f189cc914ed56c106d2e9aceb239047c8c0be7
dickensons <kuzmenkoeo@gmail.com>
2015-01-10 20:13:38 +0100

    Well, this is something we can begin to wirking with

--- Test.java
+++ Test.java
@@ -15,31 +15,35 @@ import java.awt.*;
      * First of all we need to prepare our project for the testing.
      * It would be to painful to change something in existing code and we kind 
      * of not allowed to do so for the Level class. So we won't change it, we will add
-     * code instead and make an @overload for some methods.
+     * code instead and @overload for some methods.
      * You are free to change all you think need to be change.
-     * Just not forget to shere it with us ;)
+     * Just not forget to shere it with rest us ;)
      */
     
     /* 
      * Copy and paste the folowing commented code in the Level class. Uncomment it there.
-    
-     //-----------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------
     //---------------------------------for the TEST sake----------------------------------
-    
-      private String mazespec;
-    public void setMaze(String mazespec){
-    
-        this.mazespec = mazespec;
-    
+     
+     
+        public Level(PaKman game, String newMaze)
+    {
+        this.game = game;
+        setupDefault(newMaze);
+        
+        // Initialize the number of pills
+        for (int y = 0; y < nbVertCells; y++)
+            for (int x = 0; x < nbHorzCells; x++)
+                if (maze[y][x] == Tile.PILL)
+                    pills++;
     }
     
     
 
-    public void setupDefaultTest()
+    public void setupDefault(String newMaze)
     {
+       String mazespec = newMaze;
        
-       
-
         // Copy structure into maze
         for (int i = 0; i < nbVertCells; i++)
         {
@@ -50,12 +54,75 @@ import java.awt.*;
                 maze[i][k] = charToTile(mazespec.charAt(15 * i + (29 - k)));
         }
     }
- //----------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------
  //---------------------------------end of the TEST----------------------------------
     
     */
+   
+   //And put this in PaKman class.
+   /*
+    * //-----------------------------------------------------------------------------
+    //---------------------------------for the TEST sake----------------------------------
+    
+    public PaKman(String newMaze) {
+        super(30, 33, 20, true); // Need to set the winsize, because it cannot
+                                 // be changed.
+        pacActor = new PaKActor(this);
+        setSimulationPeriod(100);
+        setTitle("TEST_PaKman");
+        addKeyListener(this);
+        addActListener(new CheckerReset());
+
+        testSetupLevel(new Level(this, newMaze));
+        
+        lives = MAX_LIVES;
+
+        // Show and activate the game window
+        show();
+        activate();
+    }
+
+    public void testSetupLevel(Level level) {
+        theLevel = level;
+        setNbHorzCells(level.getSize().x);
+        setNbVertCells(level.getSize().y);
+        level.drawLevel();
+        addActor(new PostMovementChecker(), level.getSize());
+        addActor(pacActor, level.getPakmanStart());
+        addActor(new PreMovementChecker(), level.getSize());
+
+      
+        // pakman acts after ghosts and between movement checkers, to ensure
+        // correct collision detection
+        setActOrder(Ghost.class, PreMovementChecker.class, PaKActor.class,
+                PostMovementChecker.class);
+    }
     
     
+ //----------------------------------------------------------------------
+ //---------------------------------end of the TEST----------------------------------
+    */
+   
+   /* I didn't get how to change ghostStart and PakmanStert positions.
+       So we try to use default or
+       will change pacmen and chost  start in level     
+      
+       public Location getPakmanStart() {
+        return new Location(5, 24);
+    }
+        public Location getGhostStart() {
+        return new Location(4, 15);
+    }
+   */
+   
+     /**
+     * Default constructor for test class GhostTest
+     */
+    
+    public Test() {
+    }
    
     /**
      * Test for testing Tracy. I wonder if she(he?) moves only
@@ -64,46 +131,90 @@ import java.awt.*;
      * Nothing compile here yet. Don't panic.
      */
     
+    
     //@Test
     public void tracyTest() {
     
-       PaKman testGuy = new PaKman();
+       
+       String newMaze =      //here we put our new maze.
+            "xxxxxxxxxxxxxxx" + // 0
+            "xxxxxxxxxxxxxxx" + // 1
+            "xxxxxxxxxxxxxxx" + // 2
+            "xxxxxxxxxxxxxxx" + // 3
+            "xxxxxxxxxxxxxxx" + // 4
+            "xxxxxxxxxxxxxxx" + // 5
+            "xxxxxxxxxxxxxxx" + // 6
+            "xxxxxxxxxxxxxxx" + // 7
+            "xxxxxxxxxxxxxxx" + // 8
+            "xxxxxxxxxxxxxxx" + // 9
+            "xxxxxxxxxxxxxxx" + // 10
+            "xxxxxxxxxxxxxxx" + // 11
+            "xxxxxxxxxxxxxxx" + // 12
+            "xxxxxxxxxxxxxxx" + // 13
+            "xxxxx xxxxxxxxx" + // 14
+            "xxxx  xxxxxxxxx" + // 15
+            "xxxxx xxxxxxxxx" + // 16
+            "xxxxxxxxxxxxxxx" + // 17
+            "xxxxxxxxxxxxxxx" + // 18
+            "xxxxxxxxxxxxxxx" + // 19
+            "xxxxxxxxxxxxxxx" + // 20
+            "xxxxxxxxxxxxxxx" + // 21
+            "xxxxxxxxxxxxxxx" + // 22
+            "xxxxxxxxxxxxxxx" + // 23
+            "xxxxx xxxxxxxxx" + // 24
+            "xxxxxxxxxxxxxxx" + // 25
+            "xxxxxxxxxxxxxxx" + // 26
+            "xxxxxxxxxxxxxxx" + // 27
+            "xxxxxxxxxxxxxxx" + // 28
+            "xxxxxxxxxxxxxxx" + // 29
+            "xxxxxxxxxxxxxxx" + // 30
+            "xxxxxxxxxxxxxxx" + // 31
+            "xxxxxxxxxxx..xx"   // 32 
+            ;
+       PaKman testGuy = new PaKman(newMaze);
+       
+       //Adding new Ghost in the test.
+      // Actor testTracy = new Tracy(testGuy);
+      // testTracy = testGuy.getTracy();
+       //testGuy.addActor(testTracy, new Location(5,16));
+       
+       
+        Tracy testTracy = new Tracy(testGuy);
+        testGuy.addActor(testTracy, testGuy.getLevel().getGhostStart());
+        
+       int turnedLeft = 0;
+       int turnedRight = 0;
+       
+       for(int i=0; i<1000; i++){
+       
+           
+           testTracy.act();
+           testTracy.act();
+           
+           if(testTracy.getLocation().equals(new Location(5,14))){
+               
+            turnedLeft++;
+            }
+           else 
+                if(testTracy.getLocation().equals(new Location(5,16))){
+                turnedRight++;
+            }
+           
+             testTracy.setLocation(testGuy.getLevel().getGhostStart());
+       
+    }
+     System.out.print("turnedLeft: " + turnedLeft + " turnedRight: " + turnedRight);
      
-       testGuy.getLevel().setMaze(      //here we put our new maze.
-            "xx.xxxxxxxxxxxx" + // 0
-            "xx.xxxxxxxxxxxx" + // 1
-            "..............x" + // 2
-            "xx.xxxx.xxxxx.x" + // 3
-            "xx.xxxx.xxxxx.x" + // 4
-            "xx.xxxx.xxxxx.x" + // 5
-            "xx............." + // 6
-            "xx.xxxx.xx.xxxx" + // 7
-            "xx.xxxx.xx.xxxx" + // 8
-            "xx......xx....x" + // 9
-            "xxxxxxx.xxxxx x" + // 10
-            "xxxxxxx.xxxxx x" + // 11
-            "xxxxxxx.xx     " + // 12
-            "xxxxxxx.xx xxx " + // 13
-            "xxxxxxx.xx xxx " + // 14
-            "xx     .   xx  " + // 15
-            "xxxxxxx.xx xxxx" + // 16
-            "xxxxxxx.xx xxxx" + // 17
-            "xxxxxxx.xx     " + // 18
-            "xxxxxxx.xx xxxx" + // 19
-            "xxxxxxx.xx xxxx" + // 20
-            "..............x" + // 21
-            "xx.xxxx.xxxxx.x" + // 22
-            "xx.xxxx.xxxxx.x" + // 23
-            "xx...xx........" + // 24
-            "xxxx.xx.xx.xxxx" + // 25
-            "xxxx.xx.xx.xxxx" + // 26
-            "xx......xx....x" + // 27
-            "xx.xxxxxxxxxx.x" + // 28
-            "xx.xxxxxxxxxx.x" + // 29
-            "..............." + // 30
-            ".xxxxxxxxxxxxxx" + // 31
-            ".xxxxxxxxxxxxxx"   // 32 
-            );
+        
+            
+         if(turnedRight>550){
+           fail("It turns to often right " + turnedRight);
+            }
+         else {
+           assertTrue(turnedRight<550 && turnedRight>450);
+            }
+     
         
     }
+     
 }
